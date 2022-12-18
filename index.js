4a255488c85a3d518a215236c84d385a400017b1
PayXx <sergey.dublii.94@gmail.com>
2022-12-18 15:39:41 -0600

    corrected homework 1

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const git = 'newproject'
-function rectangleeSquare(a,b) {
-    a = prompt('write the side 1 of the rectangle');
-    b = prompt('write the side 2 of the rectangle');
-    let square = a * b;
-    alert('rectangle Square is ' + square)
+const git = 'newproject';
+const rectangleeSquare = () => {
+    const a = prompt('Write the side 1 of the rectangle:');
+    const b = prompt('Write the side 2 of the rectangle:');
+    const square = a * b;
 
-}
-//rectangleeSquare()
+    alert(`Rectangle square is ${square}`);
+};
+
+rectangleeSquare();
 
 const cylinderSquare = () => {
-    const a = prompt('Write the radius of the cylinder');
-    const b = prompt("Write the height of the cylinder");
+    const a = prompt('Write the radius of the cylinder:');
+    const b = prompt('Write the height of the cylinder:');
     const result = Math.PI * Math.pow(a, 2) * b;
+
     alert(`Cylinder square is ${result}`);
 };
+
 cylinderSquare();
